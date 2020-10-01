f4db8f274693a30bdcab83e6022a3a7c1e6a87f4
Hoang Le <kuhpro@gmail.com>
2020-09-30 21:56:06 -0500

    add data for ShopPage

--- src/index.js
+++ src/index.js
@@ -4,21 +4,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './index.css';
 import HomePage from './pages/HomePage/HomePage';
-
-const Hat = props => {
-  console.log(props);
-  return <div>i'm hat</div>;
-};
+import ShopPage from './pages/ShopPage/ShopPage';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats/' component={Hat} />
-      <Route path='/shop/jackets' />
-      <Route path='/shop/sneakers' />
-      <Route path='/shop/mens' />
-      <Route path='/shop/womens' />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   );
 };
---
+++ src/pages/ShopPage/ShopPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import shopData from './ShopPage.data';
+
+const ShopPage = () => {
+  return <div>SHOP PAGE</div>;
+};
+
+export default ShopPage;
