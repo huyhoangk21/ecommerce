8ce0f92261423e515435ffcb6bb891348a08c02f
Hoang Le <kuhpro@gmail.com>
2020-09-30 23:06:09 -0500

    remove custom useForm hook

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.styles.scss';
 
 const SignIn = () => {
+  const initialInput = { email: '', password: '' };
+  const [{ email, password }, setInput] = useState(initialInput);
+
   const signIn = e => {
     e.preventDefault();
+
+    setInput(initialInput);
+  };
+
+  const changeValue = e => {
+    const { name, value } = e.target;
+    setInput({ [name]: value });
   };
 
   return (
@@ -12,9 +22,23 @@ const SignIn = () => {
       <span>Sign in with your email and password</span>
 
       <form onSubmit={signIn}>
-        <input type='email' name='email' id='email' required />
+        <input
+          type='email'
+          name='email'
+          id='email'
+          value={email}
+          onChange={changeValue}
+          required
+        />
         <label htmlFor='email'>Email</label>
-        <input type='password' name='password' id='password' required />
+        <input
+          type='password'
+          name='password'
+          id='password'
+          value={password}
+          onChange={changeValue}
+          required
+        />
         <label htmlFor='password'>Password</label>
         <input type='submit' value='Submit Form' />
       </form>
--- src/pages/SignInOutPage/SignInOutPage.js
+++ src/pages/SignInOutPage/SignInOutPage.js
@@ -4,7 +4,11 @@ import './SignInOutPage.styles.scss';
 import SignIn from '../../components/SignIn/SignIn';
 
 const SignInOutPage = () => {
-  return <SignIn />;
+  return (
+    <div className='sign-in-out'>
+      <SignIn />
+    </div>
+  );
 };
 
 export default SignInOutPage;
