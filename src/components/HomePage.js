f924621ccdde6dc7a8953b2a7f32f44da5bf7d05
Hoang Le <kuhpro@gmail.com>
2020-09-30 20:32:31 -0500

    add node-sass and styles for homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/HomePage.styles.scss';
 
 const HomePage = () => {
   return (
@@ -12,13 +13,13 @@ const HomePage = () => {
         </div>
         <div className='menu-item'>
           <div className='content'>
-            <h1 className='title'>SNEAKERS</h1>
+            <h1 className='title'>JACKETS</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
         <div className='menu-item'>
           <div className='content'>
-            <h1 className='title'>HATS</h1>
+            <h1 className='title'>SNEAKERS</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
