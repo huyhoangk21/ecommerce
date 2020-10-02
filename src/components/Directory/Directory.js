14a22662100e8589dc32da6a3c86be322c4e302f
huyhoangk21 <kuhpro@gmail.com>
2020-10-02 10:00:30 -0500

    move directory data to redux state

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Directory.styles.scss';
+import { connect } from 'react-redux';
 import MenuItem from '../MenuItem/MenuItem';
-import sections from './Directory.data';
 
-const Directory = () => {
+const Directory = ({ sections }) => {
   const renderedSections = sections.map(({ id, ...sectionProps }) => (
     <MenuItem key={id} {...sectionProps} />
   ));
 
   return <div className='directory-menu'>{renderedSections}</div>;
 };
+const mapStateToProps = ({ directory: { sections } }) => {
+  return { sections };
+};
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
