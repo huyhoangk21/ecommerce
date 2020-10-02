c5e9a4dd81187d55e811859f8fdbfc11b7078eb5
huyhoangk21 <kuhpro@gmail.com>
2020-10-02 10:03:25 -0500

    add directory selector

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Directory.styles.scss';
 import { connect } from 'react-redux';
+import { sectionsSelector } from '../../utils/directorySelector';
 import MenuItem from '../MenuItem/MenuItem';
 
 const Directory = ({ sections }) => {
@@ -10,8 +11,8 @@ const Directory = ({ sections }) => {
 
   return <div className='directory-menu'>{renderedSections}</div>;
 };
-const mapStateToProps = ({ directory: { sections } }) => {
-  return { sections };
+const mapStateToProps = state => {
+  return { sections: sectionsSelector(state) };
 };
 
 export default connect(mapStateToProps)(Directory);
