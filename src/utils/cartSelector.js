2051b30178d4b4324e8a987dcada2bdb9f6d1dcb
Hoang Le <kuhpro@gmail.com>
2020-10-01 21:48:22 -0500

    add totalPriceSelector for checkout page

--- src/utils/cartSelector.js
+++ src/utils/cartSelector.js
@@ -17,3 +17,10 @@ export const cartItemsQuantitySelector = createSelector(
   cartItems =>
     cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
 );
+
+export const cartTotalSelector = createSelector(cartItemsSelector, cartItems =>
+  cartItems.reduce(
+    (total, cartItem) => total + cartItem.quantity * cartItem.price,
+    0
+  )
+);
